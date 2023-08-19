aeaa4861491b6f8b56db19758a327bf5654a337c
Denis Stahovsky <denis.stahovsky@vantino.ch>
2023-08-19 18:41:46 +0300

    - added images for the project; - start implementing IntroPage.

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Column(
+        children: [
+          // shop name
+
+          // ison
+
+          // title
+
+          // subtitle
+
+          // get start button
+        ],
+      ),
+
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sushi_man_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SushiMapApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
